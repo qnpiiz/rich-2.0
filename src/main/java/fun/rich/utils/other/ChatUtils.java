75c1ca8eafdb2175c1c328c43dcc9b7457bd3ace
qnpiiz <abobusicsghskkg@gmail.com>
2023-08-11 05:09:17 +0300

    Improved bug with chat sending in ChatUtils

--- src/main/java/fun/rich/utils/other/ChatUtils.java
+++ src/main/java/fun/rich/utils/other/ChatUtils.java
@@ -1,12 +1,13 @@
 package fun.rich.utils.other;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.util.text.StringTextComponent;
 
 public class ChatUtils {
 
     public static String chatPrefix = "[Rich] ";
 
     public static void addChatMessage(String message) {
-        Minecraft.getInstance().ingameGUI.getChatGUI().addToSentMessages(chatPrefix.concat(message));
+        Minecraft.getInstance().ingameGUI.getChatGUI().printChatMessage(new StringTextComponent(chatPrefix.concat(message)));
     }
 }
